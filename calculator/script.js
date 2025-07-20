2ea3f3416e802ece366d9fed9530132e9e9b20de
tannu966 <tanvi3258@gmail.com>
2025-07-20 22:44:41 +0530

    added three projects- Landing page, Portfolio & Calculator

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let buttonsArray = Array.from(buttons);
+let string = '';
+buttonsArray.forEach(function(btn){
+    btn.addEventListener('click',function(event){
+        if(event.target.innerHTML =='DEL'){
+            string = string.substring(0, string.length-1);
+             display.value = string;
+        }else if(event.target.innerHTML =='AC'){
+            string = '';
+            display.value = string;
+        }else if(event.target.innerHTML =='='){
+            string = eval(string);
+            display.value = string;
+        }else{
+        string += event.target.innerHTML;
+        display.value = string;
+        }
+    });
+});
